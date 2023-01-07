e7e8e14d95fc7d797065be31aaecd82d3ee3d0f4
jes0402 <yesiik.97@gmail.com>
2023-01-07 08:35:13 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function alertLikes(){
+    alert("ninja was liked");
+}
+
+function clickHideButton(){
+    var x = document.getElementById("myButton");
+        x.style.display = "none";
+}
+
+
+function changeButton(){
+
+    document.getElementById("logginButton").innerHTML = "Logout";
+
+}
